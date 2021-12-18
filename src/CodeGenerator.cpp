bd9a6cfe206ef3ba9504fc2a1a1dbdd0d9e3ab02
Ola <aleksandra.golebiowska600@gmail.com>
2021-12-18 18:35:21 +0100

    poprawa dzielenia

--- src/CodeGenerator.cpp
+++ src/CodeGenerator.cpp
@@ -430,19 +430,37 @@ bool CodeGenerator::multiply(Variable* var1, Variable* var2) {
 bool CodeGenerator::divide(Variable* var1, Variable* var2) {
     // TODO  poteg 2 mozna pomylslec zeby tylko byly shifty
     if (var1 != nullptr && var2 != nullptr) {
+        addInstruction("RESET d"); // result
+        addInstruction("RESET g");
         makeConstant(var2->address);
         addInstruction("LOAD a"); // value2 to a
-        addInstruction("SWAP c"); // positive var2 in register c (will be remainder)
+        long long int offset1 = addInstruction("JZERO ");
+        addInstruction("JPOS 5"); // if positive don't do this
+        // +1 to register d - negate result
+        addInstruction("INC g"); // var2 was negative
+        // make positive
+        addInstruction("SWAP c"); // store in c, c == multiplier
+        addInstruction("RESET a");
+        addInstruction("SUB c");
+        addInstruction("SWAP c"); // positive var2 in register c
+
 
         makeConstant(var1->address); // in a
         addInstruction("LOAD a"); // a == val1
+        addInstruction("JPOS 5"); // var1  > 0
         addInstruction("SWAP f"); // f == val1
+        // check if var1 is negative
+        // -1 to register g
+        addInstruction("DEC g"); // var1 was negative
+        // make positive
+        addInstruction("RESET a");
+        addInstruction("SUB f");
+        addInstruction("SWAP f"); // positive var1 in register f
 
         addInstruction("RESET e");
         addInstruction("INC e");
         addInstruction("RESET h");
         addInstruction("DEC h");
-        addInstruction("RESET d"); // result
         addInstruction("RESET a");
         addInstruction("ADD c");
         addInstruction("SWAP b"); // v3 (tmp)
@@ -499,7 +517,15 @@ bool CodeGenerator::divide(Variable* var1, Variable* var2) {
         addInstruction("SWAP d");
         addInstruction("JUMP -16"); // ldivp2
 
+        long long int offset2 = addInstruction("SWAP g");
+        addInstruction("JNEG 4"); // negate
+        addInstruction("JPOS 3"); // negate
         addInstruction("SWAP d");
+        addInstruction("JUMP 3");
+        addInstruction("RESET a");
+        addInstruction("SUB d");
+
+        changeInstruction(offset1, "JZERO " + std::to_string(offset2 - offset1));
 
         return true;
     }
@@ -510,146 +536,102 @@ bool CodeGenerator::divide(Variable* var1, Variable* var2) {
 bool CodeGenerator::modulo(Variable* var1, Variable* var2) {
     if (var1 != nullptr && var2 != nullptr) {
         // quick modulo
-        if (var1->val == 0 || var2->val == 0) {
-            addInstruction("RESET a");
-            return true;
-        }
-
-        if (std::abs(var1->val) < std::abs(var2->val)) { // remainder is var1 (take into account sign of var2)
-            makeConstant(var1->address);
-            addInstruction("LOAD a"); // value1 to a
-            if ((var2->val < 0 && var1->val > 0) || (var2->val > 0 && var1->val < 0)) { // if var1 < 0 then it already has correct sign
-                // negate result
-                addInstruction("SWAP b");
-                addInstruction("RESET a");
-                addInstruction("SUB b");
-            }
-            return true;
-        }
-
-        if (var2->val == 1 || var2->val == -1) {
-            makeConstant(var1->address);
-            addInstruction("LOAD a"); // value1 to a
-            if (var2->val < 0) {
-                // negate result
-                addInstruction("SWAP b");
-                addInstruction("RESET a");
-                addInstruction("SUB b");
-            }
-            return true;
-        }
-
-        // sign of result
-        bool negate = false;
-        if (var2->val < 0) {
-            negate = true;
-        }
-
+        addInstruction("RESET d"); // result
+        addInstruction("RESET g");
+        addInstruction("RESET f");
+        addInstruction("INC g");
         makeConstant(var2->address);
         addInstruction("LOAD a"); // value2 to a
-        addInstruction("SWAP c"); // store in c, c == var2
-        if (var2->val < 0) {
-            addInstruction("RESET a");
-            addInstruction("SUB c");
-            addInstruction("SWAP c");
-        }
-        addInstruction("RESET d"); // result in d, d == 0
-        addInstruction("RESET e");
-        addInstruction("INC e"); // e == -1
+        long long int offset1 = addInstruction("JZERO ");
+        addInstruction("JPOS 5"); // if positive don't do this
+        // +1 to register d - negate result
+        addInstruction("DEC g"); // var2 was negative, if g == 0 then negate result
+        // make positive
+        addInstruction("SWAP c"); // store in c, c == multiplier
+        addInstruction("RESET a");
+        addInstruction("SUB c");
+        addInstruction("SWAP c"); // positive var2 in register c
+
+
         makeConstant(var1->address); // in a
         addInstruction("LOAD a"); // a == val1
+        addInstruction("JPOS 4"); // var1  > 0
         addInstruction("SWAP f"); // f == val1
-        if (var1->val < 0) {
-            addInstruction("RESET a");
-            addInstruction("SUB f");
-            addInstruction("SWAP f");
-        }
-
-        // find number of bits in var1 (var1 > var2)
-        int counter1 = 0;
-        long long int tmp = var1->val;
-        if (tmp < 0) {
-            tmp = -tmp;
-        }
-        while (tmp >= 1) {
-            tmp = tmp >> 1;
-            counter1++;
-        }
-
-        int counter2 = 0;
-        tmp = var2->val;
-        if (tmp < 0) {
-            tmp = -tmp;
-        }
-        while (tmp >= 1) {
-            tmp = tmp >> 1;
-            counter2++;
-        }
-
-        int counter3 = 0;
-        tmp = var1->val / var2->val;
-        if (tmp < 0) {
-            tmp = -tmp;
-        }
-        while (tmp >= 1) {
-            tmp = tmp >> 1;
-            counter3++;
-        }
-
-        // counter1 in h
-        makeConstant(counter3);
-        addInstruction("SWAP h");
-
-        // align
-        if (counter1 != counter2) {
-            // shift left var2 by difference
-            makeConstant(counter1 - counter2);
-            addInstruction("SWAP b");
-            addInstruction("SWAP c");
-            addInstruction("SHIFT b");
-            addInstruction("SWAP c"); // in c now var2 is aligned
-        }
-
-        addInstruction("SWAP h");
-        addInstruction("JZERO 16");
-        addInstruction("SWAP h");
-        // make copy of f in a
+        // check if var1 is negative
+        // make positive
         addInstruction("RESET a");
-        addInstruction("ADD f");
-        //addInstruction("SWAP f");
-        addInstruction("SUB c");
-        // check if t>=0
-        addInstruction("JNEG 3"); // t < 0 don't set N = t and don't add +1 to Q
-        // increment Q
-        addInstruction("INC d");
-        // addInstruction("JZERO 10");
-        addInstruction("SWAP f"); // N = t
-        // left N = t and left shift N by 1
-        addInstruction("SWAP f"); // N in a
-        addInstruction("SHIFT e");
-        //addInstruction("PUT");
-        addInstruction("SWAP f");
-        // left shift Q by 1
-        addInstruction("SWAP d");
-        addInstruction("SHIFT e");
-        addInstruction("SWAP d");
+        addInstruction("SUB f");
+        addInstruction("SWAP f"); // positive var1 in register f
+
+        addInstruction("RESET e");
+        addInstruction("INC e");
+        addInstruction("RESET h");
         addInstruction("DEC h");
-        addInstruction("JUMP -16");
-        // finish
-        // right shift N by 1
         addInstruction("RESET a");
-        addInstruction("DEC a");
+        addInstruction("ADD c");
+        addInstruction("SWAP b"); // v3 (tmp)
 
-        addInstruction("SWAP b");
+        addInstruction("RESET a");
+        addInstruction("ADD c");
+        addInstruction("SUB f");
+        addInstruction("RESET a");
+        addInstruction("JPOS 37"); //to end, v2>v1, result is v1, remainder is v1
+
+        addInstruction("RESET a");
+        addInstruction("ADD c");
+        addInstruction("SUB f"); // v2- v1
+        addInstruction("JPOS 5");
+        // addInstruction("JNEG 5"); // skip this, ldivpre
+        addInstruction("SWAP c");
+        addInstruction("SHIFT e");
+        addInstruction("SWAP c");
+        addInstruction("JUMP -7");
+
+        // ldivpre
+        addInstruction("SWAP c");
+        addInstruction("SHIFT h");
+        addInstruction("SWAP c");
+        addInstruction("JUMP 16"); // ldivcond
+
+        // ldivp1
         addInstruction("SWAP f");
-        addInstruction("SHIFT b");
+        addInstruction("SUB c");
         addInstruction("SWAP f");
-        if (negate) {
-            addInstruction("RESET a");
-            addInstruction("SUB f");
-        } else {
-            addInstruction("SWAP f"); // res is now in a
-        }
+
+        addInstruction("SWAP d");
+        addInstruction("SHIFT e");
+        addInstruction("INC a");
+        addInstruction("SWAP d");
+
+        // ldivp2
+        addInstruction("RESET a");
+        addInstruction("ADD c");
+        addInstruction("SUB b");
+        addInstruction("JZERO 14");
+        addInstruction("JNEG 13"); // end
+        addInstruction("SWAP c");
+        addInstruction("SHIFT h");
+        addInstruction("SWAP c");
+
+        // ldivcond
+        addInstruction("RESET a");
+        addInstruction("ADD c");
+        addInstruction("SUB f"); // v2 - v1 <=0
+        addInstruction("JZERO -18"); // ldivp1
+        addInstruction("JNEG -19"); // ldivp1
+        addInstruction("SWAP d");
+        addInstruction("SHIFT e");
+        addInstruction("SWAP d");
+        addInstruction("JUMP -16"); // ldivp2
+
+        long long int offset2 = addInstruction("SWAP g");
+        addInstruction("JZERO 3"); // negate
+        addInstruction("SWAP f");
+        addInstruction("JUMP 3");
+        addInstruction("RESET a");
+        addInstruction("SUB f");
+
+        changeInstruction(offset1, "JZERO " + std::to_string(offset2 - offset1));
 
         return true;
     }
